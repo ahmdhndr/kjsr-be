d47e940423d39b5d459c053ed6968cb7be95a607
Achmad Hendarsyah <ahmdhndrsyh@gmail.com>
2025-05-24 20:51:32 +0700

    chore: setup

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,38 @@
 /* istanbul ignore file */
 import { AllExceptionsFilter } from '@common/filters';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { env } from '@utils/env';
+import { Logger } from 'pino-nestjs';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
   app.setGlobalPrefix('api');
+  app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Swagger
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('API Docs')
     .setDescription('The list of APIs')
     .setVersion('1.0')
     .addTag('Your Great APIs')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, documentFactory, {
     customSiteTitle: 'API Docs',
     jsonDocumentUrl: 'docs/json',
   });
 
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(env.PORT);
+
+  const port = config.get<number>('PORT');
+  await app.listen(port!);
 }
 void bootstrap();
